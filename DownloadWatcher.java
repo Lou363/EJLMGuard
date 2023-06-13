fb421f7970911794e9fce09567f37825c964b3d0
louis G <l.guerin363@laposte.net>
2023-06-13 09:55:55 +0200

    first commit

---
+++ DownloadWatcher.java
@@ -0,0 +1,15 @@
+public class DownloadWatcher {
+    private static final String DOWNLOAD_DIR = "/home/username/Downloads";
+
+    public DownloadWatcher() {
+        while (true){
+        System.out.println("Heyyy");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
