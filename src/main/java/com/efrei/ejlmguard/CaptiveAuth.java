965aca50ed6d44e80a8f93d8226e4cd64494c842
Ech094 <94389928+Ech094@users.noreply.github.com>
2023-06-30 11:37:05 +0200

    pull request

---
+++ src/main/java/com/efrei/ejlmguard/CaptiveAuth.java
@@ -0,0 +1,83 @@
+package com.efrei.ejlmguard;
+
+import okhttp3.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CaptiveAuth {
+    private static boolean isWindows() {
+        String osName = System.getProperty("os.name").toLowerCase();
+        return osName.contains("windows");
+    }
+    public static void postAuth(String fwAdress) throws IOException{
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "http://"+fwAdress+":8002/index.php?zone=private_network";
+        String bodyContent = "redirurl=http%3A%2F%2Fedge-http.microsoft.com%2Fcaptiveportal%2Fgenerate_204&accept=Login";
+
+        RequestBody body = RequestBody.create(bodyContent, MediaType.parse("application/x-www-form-urlencoded"));
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .addHeader("Host", fwAdress+":8002")
+                .addHeader("Content-Length", "89")
+                .addHeader("Cache-Control", "max-age=0")
+                .addHeader("Upgrade-Insecure-Requests", "1")
+                .addHeader("Origin", "http://"+fwAdress+":8002")
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5672.93 Safari/537.36")
+                .addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+                .addHeader("Referer", "http://"+fwAdress+":8002/index.php?zone=private_network&redirurl=http%3A%2F%2Fedge-http.microsoft.com%2Fcaptiveportal%2Fgenerate_204")
+                .addHeader("Accept-Encoding", "gzip, deflate")
+                .addHeader("Accept-Language", "en-US,en;q=0.9")
+                .addHeader("Connection", "close")
+                .build();
+        Response response = client.newCall(request).execute();
+        response.close();
+    }
+
+    public static void getAuth(String fwAdress) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("http://edge-http.microsoft.com/captiveportal/generate_204")
+                .addHeader("Host", "edge-http.microsoft.com")
+                .addHeader("Cache-Control", "max-age=0")
+                .addHeader("Upgrade-Insecure-Requests", "1")
+                .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5672.93 Safari/537.36")
+                .addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+                .addHeader("Referer", "http://"+fwAdress+":8002/")
+                .addHeader("Accept-Encoding", "gzip, deflate")
+                .addHeader("Accept-Language", "en-US,en;q=0.9")
+                .addHeader("Connection", "close")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        response.close();
+    }
+    
+    public static int InternetCheck() throws IOException, InterruptedException {
+        String command = "";
+        if(isWindows()){
+            command = "ping google.com";
+        }
+        if(!isWindows()){
+            command = "ping -c 2 google.com";
+        }
+        ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
+        Process process = processBuilder.start();
+
+        int exitCode = process.waitFor();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            // Process the output if needed
+            System.out.println(line);
+        }
+        System.out.println("Exit code : " + exitCode);
+        return exitCode;
+    }
+}
