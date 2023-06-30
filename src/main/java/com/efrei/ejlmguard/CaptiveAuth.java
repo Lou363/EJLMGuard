693e9e057a84580317f9fb96ff070ff09d5e62aa
Ech094 <94389928+Ech094@users.noreply.github.com>
2023-06-30 09:59:02 +0200

    implemented requests methods

--- src/main/java/com/efrei/ejlmguard/CaptiveAuth.java
+++ src/main/java/com/efrei/ejlmguard/CaptiveAuth.java
@@ -1,54 +1,60 @@
 package com.efrei.ejlmguard;
-import java.io.DataOutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+
+import okhttp3.*;
+import java.io.IOException;
 
 public class CaptiveAuth {
-    public static void postAuth(String fwAdress){
-        try {
-            String url = "http://"+fwAdress+":8002/index.php?zone=private_network";
-            URL obj = new URL(url);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-            con.setRequestMethod("POST");
-            con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5672.93 Safari/537.36");
-            con.setRequestProperty("Accept-Language", "en-US,en;q=0.9");
-            String urlParameters = "redirurl=http%3A%2F%2Fedge-http.microsoft.com%2Fcaptiveportal%2Fgenerate_204&accept=Login";
-            con.setDoOutput(true);
-            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-            wr.writeBytes(urlParameters);
-            wr.flush();
-            wr.close();
-            int responseCode = con.getResponseCode();
-            System.out.println("Response Code: " + responseCode);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void postAuth throws IOException(String fwAdress){
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "http://"+fwAdress+":8002/index.php?zone=private_network";
+        String bodyContent = "redirurl=http%3A%2F%2Fedge-http.microsoft.com%2Fcaptiveportal%2Fgenerate_204&accept=Login";
+
+        RequestBody body = RequestBody.create(bodyContent, MediaType.parse("application/x-www-form-urlencoded"));
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .addHeader("Host", fwAdress+":8002")
+                .addHeader("Content-Length", "89")
+                .addHeader("Cache-Control", "max-age=0")
+                .addHeader("Upgrade-Insecure-Requests", "1")
+                .addHeader("Origin", "http://"+fwAdress+":8002")
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5672.93 Safari/537.36")
+                .addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+                .addHeader("Referer", "http://"+fwAdress+":8002/index.php?zone=private_network&redirurl=http%3A%2F%2Fedge-http.microsoft.com%2Fcaptiveportal%2Fgenerate_204")
+                .addHeader("Accept-Encoding", "gzip, deflate")
+                .addHeader("Accept-Language", "en-US,en;q=0.9")
+                .addHeader("Connection", "close")
+                .build();
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+        response.close();
     }
 
-    public static void getAuth() {
-        try {
-            String url = "http://edge-http.microsoft.com/captiveportal/generate_204";
-            URL obj = new URL(url);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5672.93 Safari/537.36");
-            con.setRequestProperty("Accept-Language", "en-US,en;q=0.9");
-            int responseCode = con.getResponseCode();
-            System.out.println("Response Code: " + responseCode);
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-            System.out.println(response.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void getAuth(String fwAdress) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("http://edge-http.microsoft.com/captiveportal/generate_204")
+                .addHeader("Host", "edge-http.microsoft.com")
+                .addHeader("Cache-Control", "max-age=0")
+                .addHeader("Upgrade-Insecure-Requests", "1")
+                .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5672.93 Safari/537.36")
+                .addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+                .addHeader("Referer", "http://"+fwAdress+":8002/")
+                .addHeader("Accept-Encoding", "gzip, deflate")
+                .addHeader("Accept-Language", "en-US,en;q=0.9")
+                .addHeader("Connection", "close")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println(response.code());
+        System.out.println(response.body().string());
+
+        response.close();
     }
     
 }
